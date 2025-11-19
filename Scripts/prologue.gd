33e253f12ca23123e54099c5132db108b87a00a0
Ivy Dudzik <ivydudzik@gmail.com>
2025-11-18 17:31:30 -0800

    Add Placeholder Visuals

--- Scripts/prologue.gd
+++ Scripts/prologue.gd
@@ -1,6 +1,6 @@
 extends PrologueScene
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_player: AnimationPlayer = $"Animation Player"
 
 func _ready() -> void:
 	animation_player.play("Prologue")
