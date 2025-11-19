99936df1d8d14ad787f85c710659b7958c5164e6
Ivy Dudzik <ivydudzik@gmail.com>
2025-11-19 01:44:05 -0800

    Make Shambler Update Continuously

--- Scripts/shambler.gd
+++ Scripts/shambler.gd
@@ -5,18 +5,17 @@ var targets: Array[Node2D] = []
 var target: Node2D = null
 
 func _process(delta: float) -> void:
+	target = get_closest_target(targets)
 	if target:
 		global_position = global_position + (target.global_position - global_position).normalized() * delta * speed
 
 
 func _on_light_detector_area_entered(area: Area2D) -> void:
 	targets.append(area)
-	target = get_closest_target(targets)
 
 
 func _on_light_detector_area_exited(area: Area2D) -> void:
 	targets.erase(area)
-	target = get_closest_target(targets)
 
 func get_closest_target(target_array: Array[Node2D]):
 	var closest_target: Node2D = null
