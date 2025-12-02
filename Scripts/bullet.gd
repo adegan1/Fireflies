9b9ff760df085bc5e91d2f6daefc163cfd7630b8
Gareg <reg_ga@farmorereal.com>
2025-12-01 21:20:37 -0800

    created working gunner implementation in level 1

---
+++ Scripts/bullet.gd
@@ -0,0 +1,25 @@
+extends RigidBody2D
+
+@export var player: Node2D
+@export var lifetime: float = 10.0
+
+func _ready() -> void:
+	if (player == null):
+		player = get_node("/root/Main/Level 1/Player")
+	
+	linear_velocity = (player.global_position - self.global_position).normalized() * get_parent().bulletSpeed
+	
+	var timer := Timer.new()
+	add_child(timer)
+	timer.wait_time = lifetime
+	timer.one_shot = true
+	timer.start()
+	timer.timeout.connect(die)
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("die"):
+		body.die()
+
+func die() -> void:
+	queue_free()
